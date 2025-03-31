d7d8ce9ae7dd009cb1a59c3e4d828f6c0bb357c7
Mark-botx <marktatebusinessmail@gmail.com>
2025-03-31 13:58:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ API_HASH = os.environ.get("API_HASH", "35b9c34a2b29b20bdae81d82e2863cec")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002517187425"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "5119055997 7012709838 5560200410"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7012709838 5119055997"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
@@ -50,8 +50,8 @@ TUT_VID = os.environ.get("TUT_VID","https://t.me/howtoopenprmlink")
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʟʟᴏ {first}\n\n ɪ ᴀᴍ ᴍᴜʟᴛɪ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ , ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ » @all_botx</b>")
 try:
-    ADMINS=[5560200410]
-    for x in (os.environ.get("ADMINS", "7012709838 5560200410 5119055997").split()):
+    ADMINS=[7012709838]
+    for x in (os.environ.get("ADMINS", "7012709838 5119055997").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -72,7 +72,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!\n\n» ᴍʏ ᴏᴡɴᴇʀ : @Adult_77"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(5119055997)
+ADMINS.append(7012709838)
 
 LOG_FILE_NAME = "codeflixbots.txt"
 
